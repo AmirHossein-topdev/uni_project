748edc3d46cec82000992fb2784fe5e59a930495
AmirHossein <mohsenifar1383@gmail.com>
2025-12-03 21:58:49 +0330

    update boxe54

--- frontend/src/pages/forgot.jsx
+++ frontend/src/pages/forgot.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import CommonBreadcrumb from "@/components/breadcrumb/common-breadcrumb";
 import ForgotArea from "@/components/login-register/forgot-area";
 
 const ForgotPage = () => {
   return (
     <>
-      <CommonBreadcrumb
-        title="Forgot Password"
-        subtitle="Reset Password"
-        center={true}
-      />
       <ForgotArea />
     </>
   );
