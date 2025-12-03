f79464ca1de0c6c7f5a1644b3a42fb6d496d506a
AmirHossein <mohsenifar1383@gmail.com>
2025-12-03 22:02:23 +0330

    update boxe7

--- frontend/src/pages/register.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import RegisterArea from "@/components/login-register/register-area";
-
-const RegisterPage = () => {
-  return <RegisterArea />;
-};
-
-export default RegisterPage;
